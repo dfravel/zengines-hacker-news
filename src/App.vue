45d598981f5742deac4dc63807401136ef5f630e
Dave Fravel <dave@bluegear.io>
2023-01-03 13:46:08 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex items-center justify-center h-screen text-2xl text-green-700">Welcome to the Zengines Hacker News Clone</div>
+</template>
+
+<style scoped></style>
